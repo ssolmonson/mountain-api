08789b350b636e4c3ccc3a484ac04a1297527b32
Scott Solmonson <ssolmonson63@gmail.com>
2020-02-16 15:14:57 -0500

    Edit README
    Created a better README with links to deployment and the other client
    repo.

--- app/serializers/mountain_serializer.rb
+++ app/serializers/mountain_serializer.rb
@@ -1,5 +1,5 @@
 class MountainSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :vertical_drop, :difficulty, :conditions, :runs_made, :date_of_visit, :user_mountain
+  attributes :id, :name, :location, :vertical_drop, :difficulty, :conditions, :runs_made, :date_of_visit, :user_id, :user_mountain
 
   def user_mountain
     scope == object.user
